fffe544506d467bcaab4c2f6b1401796dd1c223d
kulitis <50826616+kulitis@users.noreply.github.com>
2021-05-18 08:05:38 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-		  I was changed on the feature brach</br>
+		  I was changed on the feature brach<br/>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
